6768983a89117fa8465fb287dfd51722f8bdc7f5
Judisoft <kumjude09@gmail.com>
2022-09-08 17:27:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
  * main -  prints the string
  *
- * Description: uses main function
- * This program prints Programming is like building a multilingual puzzle.
- * Returns the string 'Programming is like building a multilingual puzzle'
- **/
+ * Description: prints Programming is like building a multilingual puzzle.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
