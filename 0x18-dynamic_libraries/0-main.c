e349b8a90688b1f449dffa83256f0a5c04f76794
Judisoft <kumjude09@gmail.com>
2022-12-12 15:18:26 -0800

    create dynamic library

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+
+ * main - check the code for Holberton School students.
+
+ *
+
+ * Return: Always EXIT_SUCCESS.
+
+ */
+
+int main(void)
+
+{
+
+	printf("%d\n", _strlen("libdynamic"));
+
+	return (EXIT_SUCCESS);
+
+}
+
